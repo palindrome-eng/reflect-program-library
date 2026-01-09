4073ad0ea1fa19c096ab822c9f3d7ddf560f4e8b
Daniel Pavel <daniel.pavel19@gmail.com>
2026-01-09 13:52:03 +0200

    fixes:
    - fix 0.0.0.0. The newer agave-validator (3.0.13) doesn't accept 0.0.0.0
    as a valid bind address for the gossip node.
    - formats

--- rlp/programs/rlp/src/states/liquidity_pool.rs
+++ rlp/programs/rlp/src/states/liquidity_pool.rs
@@ -1,13 +1,11 @@
-use anchor_lang::prelude::*;
-use anchor_spl::token::{
-    mint_to, transfer, Mint, MintTo, Token, TokenAccount, Transfer
-};
-use anchor_spl::associated_token::get_associated_token_address;
 use crate::constants::*;
-use crate::helpers::OraclePrice;
-use spl_math::precise_number::PreciseNumber;
 use crate::errors::InsuranceFundError;
+use crate::helpers::OraclePrice;
 use crate::states::*;
+use anchor_lang::prelude::*;
+use anchor_spl::associated_token::get_associated_token_address;
+use anchor_spl::token::{mint_to, transfer, Mint, MintTo, Token, TokenAccount, Transfer};
+use spl_math::precise_number::PreciseNumber;
 
 #[derive(InitSpace)]
 #[account]
@@ -31,14 +29,14 @@ impl LiquidityPool {
         if asset_amount > 0 {
             transfer(
                 CpiContext::new(
-                    token_program.to_account_info(), 
-                    Transfer { 
-                        from: asset_user_account.to_account_info(), 
-                        to: asset_pool.to_account_info(), 
-                        authority: signer.to_account_info()
-                    }
-                ), 
-                asset_amount
+                    token_program.to_account_info(),
+                    Transfer {
+                        from: asset_user_account.to_account_info(),
+                        to: asset_pool.to_account_info(),
+                        authority: signer.to_account_info(),
+                    },
+                ),
+                asset_amount,
             )?
         }
 
@@ -52,93 +50,96 @@ impl LiquidityPool {
         settings: &Account<Settings>,
         clock: &Clock,
     ) -> Result<PreciseNumber> {
-        let mut total_pool_value = PreciseNumber::new(0)
-            .ok_or(crate::errors::InsuranceFundError::MathOverflow)?;
-        
+        let mut total_pool_value =
+            PreciseNumber::new(0).ok_or(crate::errors::InsuranceFundError::MathOverflow)?;
+
         require!(
             remaining_accounts.len() == settings.assets as usize * 3,
             crate::errors::InsuranceFundError::InvalidInput
         );
-        
+
         let mut i = 0;
         while i < remaining_accounts.len() {
             let token_account_info = &remaining_accounts[i];
-            
+
             require!(
                 token_account_info.owner == &anchor_spl::token::ID,
                 crate::errors::InsuranceFundError::InvalidInput
             );
-    
-            let token_account = TokenAccount::try_deserialize(&mut token_account_info.try_borrow_mut_data()?.as_ref())
-                .map_err(|_| crate::errors::InsuranceFundError::InvalidInput)?;
-    
+
+            let token_account = TokenAccount::try_deserialize(
+                &mut token_account_info.try_borrow_mut_data()?.as_ref(),
+            )
+            .map_err(|_| crate::errors::InsuranceFundError::InvalidInput)?;
+
             require!(
                 token_account.owner == liquidity_pool.key(),
                 crate::errors::InsuranceFundError::InvalidInput
             );
-    
+
             let asset_info = &remaining_accounts[i + 1];
-            
+
             require!(
                 asset_info.owner == &crate::ID,
                 crate::errors::InsuranceFundError::InvalidInput
             );
-            
+
             let asset = Asset::try_deserialize(&mut asset_info.try_borrow_mut_data()?.as_ref())
                 .map_err(|_| crate::errors::InsuranceFundError::InvalidInput)?;
-    
+
             require!(
                 asset.mint == token_account.mint,
                 crate::errors::InsuranceFundError::InvalidInput
             );
-            
+
             let (expected_asset_pda, _) = Pubkey::find_program_address(
                 &[
                     crate::constants::ASSET_SEED.as_bytes(),
-                    &asset.mint.to_bytes()
+                    &asset.mint.to_bytes(),
                 ],
-                &crate::ID
+                &crate::ID,
+            );
+
+            // Verify this is the correct PDA for asset
+            require!(
+                asset_info.key() == expected_asset_pda,
+                crate::errors::InsuranceFundError::InvalidInput
             );
 
             // Verify this is the correct associated token account for the liquidity pool and asset
-            let expected_pool_token_account = get_associated_token_address(
-                &liquidity_pool.key(), 
-                &asset.mint
-            );
+            let expected_pool_token_account =
+                get_associated_token_address(&liquidity_pool.key(), &asset.mint);
 
             require!(
                 token_account_info.key() == expected_pool_token_account,
                 crate::errors::InsuranceFundError::InvalidInput
             );
-    
-            require!(
-                asset_info.key() == expected_asset_pda,
-                crate::errors::InsuranceFundError::InvalidInput
-            );
-    
+
             let oracle_info = &remaining_accounts[i + 2];
-    
+
             require!(
                 oracle_info.key() == *asset.oracle.key(),
                 crate::errors::InsuranceFundError::InvalidInput
             );
-    
-            let asset_price = asset.get_price(oracle_info, clock)
+
+            let asset_price = asset
+                .get_price(oracle_info, clock)
                 .map_err(|_| crate::errors::InsuranceFundError::InvalidInput)?;
-    
+
             let token_balance = token_account.amount;
             if token_balance > 0 {
                 let token_value_precise = PreciseNumber::new(asset_price.mul(token_balance)?)
                     .ok_or(crate::errors::InsuranceFundError::MathOverflow)?;
-                total_pool_value = total_pool_value.checked_add(&token_value_precise)
+                total_pool_value = total_pool_value
+                    .checked_add(&token_value_precise)
                     .ok_or(crate::errors::InsuranceFundError::MathOverflow)?;
             }
-    
+
             i += 3;
         }
-    
+
         Ok(total_pool_value)
-    } 
+    }
 
     pub fn calculate_lp_tokens_on_deposit(
         &self,
@@ -162,7 +163,6 @@ impl LiquidityPool {
                 .checked_div(&total_pool_value)
                 .ok_or(crate::errors::InsuranceFundError::MathOverflow)?;
 
-            
             deposit_ratio
                 .to_imprecise()
                 .ok_or(crate::errors::InsuranceFundError::MathOverflow)?
@@ -184,20 +184,20 @@ impl LiquidityPool {
         let signer_seeds = &[
             LIQUIDITY_POOL_SEED.as_bytes(),
             &self.index.to_le_bytes(),
-            &[self.bump]
+            &[self.bump],
         ];
 
         mint_to(
             CpiContext::new_with_signer(
-                token_program.to_account_info(), 
-                MintTo { 
-                    mint: lp_token.to_account_info(), 
-                    to: lockup_lp_token_vault.to_account_info(), 
-                    authority: liquidity_pool.to_account_info() 
+                token_program.to_account_info(),
+                MintTo {
+                    mint: lp_token.to_account_info(),
+                    to: lockup_lp_token_vault.to_account_info(),
+                    authority: liquidity_pool.to_account_info(),
                 },
-                &[signer_seeds]
-            ), 
-            amount
+                &[signer_seeds],
+            ),
+            amount,
         )?;
 
         Ok(())
